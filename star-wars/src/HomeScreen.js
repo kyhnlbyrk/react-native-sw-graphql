113123fc38e3124e0c84ce236a9a7624c25dd14d
kayahan <albayrakkayahan@gmail.com>
2021-10-14 18:14:50 +0300

    shake event bug fix

--- star-wars/src/HomeScreen.js
+++ star-wars/src/HomeScreen.js
@@ -1,12 +1,22 @@
 import React, { useEffect, useState } from 'react'
 import { Text, View } from 'react-native'
-
+import { ShakeHandler } from './handlers/ShakeHandler'
 
 const HomeScreen = (props) => {
   const { planets } = props;
   const [choosenPlanet, setChoosenPlanet] = useState({});
 
+  useEffect(() => {
+    const index = getRandomInt(0, planets.length);
+    setChoosenPlanet(planets[index]);
+    ShakeHandler.addListener(() => {
 
+      const index = getRandomInt(0, planets.length);
+      setChoosenPlanet(planets[index]);
+      //add your code here
+      console.log('Shake Shake Shake');
+    });
+  }, []);
 
   const getRandomInt = (min, max) => {
     min = Math.ceil(min);
